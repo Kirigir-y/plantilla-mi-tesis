e8d95f02ae2e7d5073de75d81eb9b88f638baa19
Kirigir-y <fer.gonzalez@duocuc.cl>
2026-03-23 15:53:04 -0300

    peque cambio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // Descomenta y edita esto cuando subas a GitHub Pages:
-  // site: 'https://TU_USUARIO.github.io',
-  // base: '/TU_REPOSITORIO',
+ site: 'https://kirigir-y.github.io',
+  base: '/plantilla-mi-tesis',
 });
